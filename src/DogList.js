cd1adb6e6005e3e2270905f97fada0ad250e536d
Joshua Kaluski <joshua@kaluski.com>
2019-06-07 11:55:10 -0400

    Initial commit of barebones demo.

---
+++ src/DogList.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './DogList.css';
+
+class DogList extends Component {
+    render() {
+        return (
+            <div className="DogList">
+                <h1 className="display-1 text-center">Dog List!</h1>
+                <div className="container">
+                    <div className="row">
+                        {this.props.dogs.map( dog => (
+                            <div key={dog.name} className="DogList-Dog col-md-6 col-lg-4 text-center">
+                                <img src={dog.src} alt={dog.name}/>
+                                <h3>{dog.name}</h3>
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DogList;
